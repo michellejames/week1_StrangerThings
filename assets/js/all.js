2be766147e4b2ddf1487ed811dc67e777731dcc8
michellejames <shellyjames90@yahoo.com>
2017-07-18 12:14:52 -0400

    Initial Setup

---
+++ assets/js/all.js
@@ -0,0 +1,12 @@
+TweenMax.fromTo(".lights", 3, {delay: 2, opacity: 0, scale: .1}, {opacity: 1, scale: .5});
+
+TweenMax.fromTo(".st-logo", 5, { delay: 2, opacity: 0, scale: .1 }, { opacity: 1, scale: .5});
+
+TweenMax.fromTo(".season2", 3, {delay: 2, opacity: 0}, {opacity: 1});
+
+// TweenMax.from(".netflix", 3, {delay: 2, scale: .3});
+
+
+
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJhbGwuanMiLCJzb3VyY2VzQ29udGVudCI6WyJUd2Vlbk1heC5mcm9tVG8oXCIubGlnaHRzXCIsIDMsIHtkZWxheTogMiwgb3BhY2l0eTogMCwgc2NhbGU6IC4xfSwge29wYWNpdHk6IDEsIHNjYWxlOiAuNX0pO1xuXG5Ud2Vlbk1heC5mcm9tVG8oXCIuc3QtbG9nb1wiLCA1LCB7IGRlbGF5OiAyLCBvcGFjaXR5OiAwLCBzY2FsZTogLjEgfSwgeyBvcGFjaXR5OiAxLCBzY2FsZTogLjV9KTtcblxuVHdlZW5NYXguZnJvbVRvKFwiLnNlYXNvbjJcIiwgMywge2RlbGF5OiAyLCBvcGFjaXR5OiAwfSwge29wYWNpdHk6IDF9KTtcblxuLy8gVHdlZW5NYXguZnJvbShcIi5uZXRmbGl4XCIsIDMsIHtkZWxheTogMiwgc2NhbGU6IC4zfSk7XG5cblxuXG4iXX0=
---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+TweenMax.fromTo(".lights", 3, {delay: 2, opacity: 0, scale: .1}, {opacity: 1, scale: .5});
+
+TweenMax.fromTo(".st-logo", 5, { delay: 2, opacity: 0, scale: .1 }, { opacity: 1, scale: .5});
+
+TweenMax.fromTo(".season2", 3, {delay: 2, opacity: 0}, {opacity: 1});
+
+// TweenMax.from(".netflix", 3, {delay: 2, scale: .3});
+
+
+
